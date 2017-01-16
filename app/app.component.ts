fc543397d9e82c42a6444975fe836d8e1ed1f682
Robert Mayer <rwlmayer@gmail.com>
2017-01-16 07:42:27 +0100

    Added sidebar and top navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,9 +7,46 @@ import { NavbarComponent } from './navbar/navbar.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'App works!'; 
+  menuHidden: boolean = false;
+
   constructor() 
   {
     
   }
+
+  clicked() {
+    this.menuHidden = !this.menuHidden;
+    console.log(this.menuHidden);
+  }
 }
+
+
+// $("#menu-toggle").click(function(e) {
+//         e.preventDefault();
+//         $("#wrapper").toggleClass("toggled");
+//     });
+//      $("#menu-toggle-2").click(function(e) {
+//         e.preventDefault();
+//         $("#wrapper").toggleClass("toggled-2");
+//         $('#menu ul').hide();
+//     });
+ 
+    //  function initMenu() {
+    //   $('#menu ul').hide();
+    //   $('#menu ul').children('.current').parent().show();
+    //   //$('#menu ul:first').show();
+    //   $('#menu li a').click(
+    //     function() {
+    //       var checkElement = $(this).next();
+    //       if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    //         return false;
+    //         }
+    //       if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    //         $('#menu ul:visible').slideUp('normal');
+    //         checkElement.slideDown('normal');
+    //         return false;
+    //         }
+    //       }
+    //     );
+    //   }
+    // $(document).ready(function() {initMenu();});
